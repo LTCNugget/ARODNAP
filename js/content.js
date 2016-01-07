2992628dbf82a17a9ebdb216969298f0914fddbf
LTCNugget <kylebrott@gmail.com>
2016-01-07 18:18:48 -0500

    fixed issue #5

--- js/content.js
+++ js/content.js
@@ -1,27 +1,23 @@
 // content.js
 
-var isThereNoSrc;
+var areAllSrcPresent = true;
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {     // listens for messages
-  console.log("message listener activated");
   if (request.name === "grabInfo") {
-    console.log("request.name equals grabInfo");
-    var song = document.getElementsByClassName("songTitle")[0].innerHTML;
-    var artist = document.getElementsByClassName("artistSummary")[0].innerHTML;
-    var album = document.getElementsByClassName("albumTitle")[0].innerHTML;
-    var art = document.getElementsByClassName("art");
     if (song !== undefined && artist !== undefined && album !== undefined && art[1].src !== undefined) {     // checks to see if all the required info is present
+      var song = document.getElementsByClassName("songTitle")[0].innerHTML;
+      var artist = document.getElementsByClassName("artistSummary")[0].innerHTML;
+      var album = document.getElementsByClassName("albumTitle")[0].innerHTML;
+      var art = document.getElementsByClassName("art");
       for (var i = art.length - 1; i > 0; i--) {     // runs through art[] looking for elements with no src attributes (excluding art[0])
-        console.log("");
-        if (art[i].src === undefined) {
-          console.log("\"%s\" by %s on the album %s. Please refresh the page and run again to get album art.", song, artist, album);
-          var isThereNoSrc = false;
+        if (art[i].src === "") {
+          console.log("\"%s\" by %s on the album %s.", song, artist, album);
+          console.error("Album art not listed: Could not be found");
+          areAllSrcPresent = false;
           break;
         }
       }
-      if (isThereNoSrc === false) {
-        console.log("\"%s\" by %s on the album %s \(%s\).", song, artist, album, art[1].src);     // expected result
-      }
-    } else { console.log("Not all the information is present or valid."); }
+      if (areAllSrcPresent === true) { console.log("\"%s\" by %s on the album %s \(%s\).", song, artist, album, art[1].src); }     // perfect result
+    } else { console.error("Not all the information is present or valid."); }
   }
 });
