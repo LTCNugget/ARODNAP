798b6d8f0b59e746ad4aee3f1f620ca63f725cf2
LTCNugget <kylebrott@gmail.com>
2016-01-08 16:02:37 -0500

    fixed #6, fixed #7, can now deal with radio switch

--- js/content.js
+++ js/content.js
@@ -1,18 +1,19 @@
 // content.js
 
-var areAllSrcPresent = true;
+var art = "";
+var whereIs2ndNoSrc = "";
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {     // listens for messages
   if ( request.name === "grabInfo" ) {
-    if ( $(".art")[0].src === "" ) { $( $(".art")[0].remove() ); }
     if ( $(".songTitle") !== null && $(".artistSummary") !== null && $(".albumTitle") !== null && $(".art") !== null ) {     // checks to see if all the required info is present
-      for ( var i = $(".art").length - 1; i >= 0; i-- ) { if ( $(".art")[i].src === "" ) { areAllSrcPresent = false; } }     // runs through $(".art") looking for elements with no src attributes
-      if ( areAllSrcPresent === true ) { console.log("\"%s\" by %s on the album %s \(%s\).", $(".songTitle")[0].innerHTML, $(".artistSummary")[0].innerHTML, $(".albumTitle")[0].innerHTML, $(".art")[0].src); }     // perfect result
-      else if ( areAllSrcPresent === false ) {
-        $( $(".art")[0].remove() );
-        console.log("\"%s\" by %s on the album %s \(%s\).", $(".songTitle")[0].innerHTML, $(".artistSummary")[0].innerHTML, $(".albumTitle")[0].innerHTML, $(".art")[0].src);
-        $( $(".art")[0].remove() );
-      }
+      for (var i = 1; i <= $(".art").length - 1; i++) { if ( $(".art")[i].src === "" ) { whereIs2ndNoSrc = i; } }
+      if (whereIs2ndNoSrc !== "") { for (var i = whereIs2ndNoSrc - 1; i >= 0; i--) {
+        $( $(".art")[i].remove() );
+        var whereIs2ndNoSrc = ""; } }
+      var art = $(".art")[1].src
+      console.log("\"%s\" by %s on the album %s \(%s\).", $(".songTitle")[0].innerHTML, $(".artistSummary")[0].innerHTML, $(".albumTitle")[0].innerHTML, art);     // expected result
+      var art = "";
+      $( $(".art")[1].remove() );
     } else { console.error("Not all the information is present or valid."); }
   }
 });
